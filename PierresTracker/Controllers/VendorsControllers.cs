1080a36e6eaafdd18582c8af00b2e93263d99c73
Diego Serafico <dseraficohernandez@gmail.com>
2021-10-01 13:09:18 -0700

    add paths

--- PierresTracker/Controllers/VendorsControllers.cs
+++ PierresTracker/Controllers/VendorsControllers.cs
@@ -1,11 +1,22 @@
+using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using PierresTracker.Models;
-using System.Collections.Generic;
 
 namespace PierresTracker.Controllers
 {
   public class VendorsController : Controller
   {
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      return View();
+    }
 
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
   }
 }

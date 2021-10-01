16217cb157fb52a555d10b05dedb77e882be10f4
Diego Serafico <dseraficohernandez@gmail.com>
2021-10-01 16:40:56 -0700

    add functionality to display vendors and route to home

--- PierresTracker/Controllers/VendorsControllers.cs
+++ PierresTracker/Controllers/VendorsControllers.cs
@@ -10,13 +10,21 @@ namespace PierresTracker.Controllers
     [HttpGet("/vendors")]
     public ActionResult Index()
     {
-      return View();
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
     }
 
     [HttpGet("/vendors/new")]
     public ActionResult New()
     {
       return View();
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string vendorDescription)
+    {
+      Vendor newVendor = new Vendor(vendorName, vendorDescription);
+      return RedirectToAction("Index", "Home");
     }
   }
 }
